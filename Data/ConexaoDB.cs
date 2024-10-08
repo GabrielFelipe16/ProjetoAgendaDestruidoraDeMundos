73009e2111d0b7f519a2d0eca19fd074c37ed427
Gabriel Belentani Felipe <gabriel.b.felipe@aluno.senai.br>
2024-10-08 17:25:28 -0300

    adicionando pastas para organização e criando classes de conexão com banco de dados

---
+++ Data/ConexaoDB.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAgendaDestruidoraDeMundos.Data
+{
+    static internal class ConexaoDB
+    {
+        static public MySqlConnection CriaConexao() {
+
+            //String com informações para conexão
+            string strConn = "Server=localhost;Database=dbagenda;User ID=root;password=root;";
+
+            //iniciando uma conexão com o banco de dados
+            MySqlConnection conn = new MySqlConnection(strConn);
+
+            return conn;
+
+        }
+    }
+}
